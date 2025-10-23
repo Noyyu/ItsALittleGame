e4cffc9758959fa820280604eca041b87b71afee
Noyyu <Nikki.Norberg@hotmail.com>
2025-10-23 23:15:25 +0200

    Ascii animations
    Added animations for the player movement.

--- Game.cs
+++ Game.cs
@@ -15,18 +15,24 @@ internal class Game
 
 
     //Timing management
-    private int timeSinceLastUpdateMs = 0;          // Time since last update
-    private const int preferedLoopRunTimeMs = 60;   // Runs every 60 ms
-    private long loopRunTimeMs = 0;                 // Time
+    private int timeSinceLastUpdateMs = 0;                   // Actual time since last update
+    private const int preferedTimeSinceLastUpdateMs = 60;    // Preffered time we want in between updates
+    private long loopRunTimeMs = 0;                          // Used to calculate time in between updates. 
 
     private readonly Stopwatch clock = Stopwatch.StartNew();
     
     private Player player = new Player();
     private int dirX = 0, dirY = 0;
 
+    int screenLeft = 1, screenTop = 1;
+    int screenRight, screenBottom;
 
     public Game()
     {
+        screenRight = Console.WindowWidth - 2;
+        screenBottom = Console.WindowHeight - 2;
+        player.SetPlayerScreenBounds(screenTop, screenRight, screenLeft, screenBottom);
+
         Console.CursorVisible = false;
         // Anpassa gärna men håll det rimligt
         Console.SetWindowSize(120, 40);
@@ -35,33 +41,42 @@ internal class Game
 
     public void Update()
     {
-        // ? means If this, then this
-        // : means "Else"
-        dirX = IsDown(left) || IsDown(a) ? -1
-             : IsDown(right) || IsDown(d) ? 1 : 0;
 
-        dirY = IsDown(up) || IsDown(w) ? -1
-             : IsDown(down) || IsDown(s) ? 1 : 0;
+        GetPlayerDirection();
+        CalculateTimeSinceLastUpdate();
+        UpdatePlayerPosition();
 
-
-        //Clock! It puts the runtime of the loop in "timeAccumulatorMs".
-        var now = clock.ElapsedMilliseconds; // Gets current time since game started
-        timeSinceLastUpdateMs += (int)(now - loopRunTimeMs); //Calculates how much time its been since the last time this was updated. 
-        loopRunTimeMs = now; //Stores "time since game started" to calculate the next loop time. 
-
-        if ((dirX != 0 || dirY != 0) && timeSinceLastUpdateMs >= preferedLoopRunTimeMs) //If we have registered a direction AND if its been at least a prefered amount of time since the last update. 
-        {
-            player.Move(dirX, dirY);
-            timeSinceLastUpdateMs = 0; //Resets the timer
-        }
     }
 
     public void Render()
     {
-        player.PlacePlayer();
+        DrawBorder();
+        player.AnimatePlayer(dirX, dirY);
+        //player.PlacePlayer();
     }
 
- 
+    void DrawBorder()
+    {
+        // Edges
+        Console.SetCursorPosition(screenLeft - 1, screenTop - 1);     Console.Write('╔'); // Upper Left
+        Console.SetCursorPosition(screenRight + 1, screenTop - 1);    Console.Write('╗'); // Upper Right
+        Console.SetCursorPosition(screenLeft -1, screenBottom + 1);   Console.Write('╚'); // Bottom Left
+        Console.SetCursorPosition(screenRight + 1, screenBottom +1);  Console.Write('╝'); // Bottom Right
+
+        // top & bottom
+        for (int x = screenLeft; x <= screenRight; x++)
+        {
+            Console.SetCursorPosition(x, screenTop - 1);    Console.Write('═');
+            Console.SetCursorPosition(x, screenBottom + 1); Console.Write('═');
+        }
+        // sides
+        for (int y = screenTop; y <= screenBottom; y++)
+        {
+            Console.SetCursorPosition(screenLeft -1, y);   Console.Write('║');
+            Console.SetCursorPosition(screenRight + 1, y); Console.Write('║');
+        }
+    }
+
     private static bool IsDown(int vKey)
     {
         short keyState = GetAsyncKeyState(vKey);  // Ask windows for the key status
@@ -69,4 +84,29 @@ internal class Game
         return isPressed;                          
     }
 
+    private void UpdatePlayerPosition()
+    {
+        if ((dirX != 0 || dirY != 0) && timeSinceLastUpdateMs >= preferedTimeSinceLastUpdateMs) //If we have registered a direction AND if its been at least a prefered amount of time since the last update. 
+        {
+            player.Move(dirX, dirY);
+            timeSinceLastUpdateMs = 0; //Resets the timer
+        }
+    }
+    private void CalculateTimeSinceLastUpdate()
+    {
+        //Clock! It puts the runtime of the loop in "timeAccumulatorMs".
+        var now = clock.ElapsedMilliseconds; // Gets current time since game started
+        timeSinceLastUpdateMs += (int)(now - loopRunTimeMs); //Calculates how much time its been since the last time this was updated. 
+        loopRunTimeMs = now; //Stores "time since game started" to calculate the next loop time. 
+    }
+    private void GetPlayerDirection()
+    {
+        dirX = IsDown(left)  || IsDown(a) ? -1
+             : IsDown(right) || IsDown(d) ? 1
+             : 0;
+
+        dirY = IsDown(up)   || IsDown(w) ? -1
+             : IsDown(down) || IsDown(s) ? 1
+             : 0;
+    }
 }
--- Player.cs
+++ Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,26 @@ namespace ItsALittleGame
 {
     internal class Player
     {
+        private int screenTop;
+        private int screenRight;
+        private int screenLeft;
+        private int screenBottom;
         public int X { get; set; } = 10;
         public int Y { get; set; } = 5;
 
         private int prevY = 10;
         private int prevX = 5;
         public int Health { get; set; }
-        int WalkingSpeed { get; set; } = 2;
+        int WalkingSpeed { get; set; } = 1;
+
+        int runFrames = 6;
+        int currentRunFrame;
+        int climbFrames = 2;
+        int currentClimbFrame;
+        int jumpFrames = 3;
+        int currentJumpFrame;
+
+        int prevSprite;
 
         char tempSymbol = 'h';
 
@@ -24,55 +38,350 @@ namespace ItsALittleGame
 
         }
 
-        public void PlacePlayer()
+        public void SetPlayerScreenBounds(int top, int right, int left, int bottom)
+        {
+            screenBottom = bottom;
+            screenRight = right;
+            screenLeft = left;
+            screenTop = top;
+        }
+
+        void WriteColor(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+        public void AnimatePlayer(int dirX, int dirY)
         {
             Console.SetCursorPosition(prevX, prevY);
-
-            //Handle everything
-            if (X < Console.WindowWidth && X > 0 && Y < Console.WindowHeight && Y > 0)
-            {
-                Console.Write(" ");
-                Console.SetCursorPosition(X, Y);
-                Console.Write(tempSymbol);
-            }
-            //If something isn't right
-            else
-            {
-                if (X > Console.WindowWidth)
-                {
-                    X = Console.WindowWidth - 1;
-                }
-                else if (X < 0)
-                {
-                    X = 1;
-                }
-
-                if (Y > Console.WindowHeight)
-                {
-                    Y = Console.WindowHeight - 1;
-                }
-                else if (Y < 0)
-                {
-                    Y = 1;
-                }
-
-                Console.Write(" ");
-                Console.SetCursorPosition(X, Y);
-                Console.Write(tempSymbol);
-            }
-
+            Console.Write("      ");
+            Console.SetCursorPosition(prevX, prevY + 1);
+            Console.Write("      ");
+            Console.SetCursorPosition(prevX, prevY + 2);
+            Console.Write("      ");
             prevY = Y;
             prevX = X;
+
+            // 0 walk right
+            // 1 walk left
+            // 2 climb
+            // 3 jump
+
+            //Check 
+
+            if (dirX == 1) //If player is moving right
+            {
+                if (prevSprite == 0)
+                {
+                    currentRunFrame = (currentRunFrame + 1 > runFrames) ? 1 : currentRunFrame + 1;                    
+                }
+                PlayerFrames(0, currentRunFrame);
+                prevSprite = 0;
+            }
+            else if (dirX == -1) //If the player is moving left
+            {
+                if (prevSprite == 1)
+                {
+                    currentRunFrame = (currentRunFrame + 1 > runFrames) ? 1 : currentRunFrame + 1;
+                }
+                PlayerFrames(1, currentRunFrame);
+                prevSprite = 1;
+            }
+            else if (dirY == -1)
+            {
+                if (prevSprite == 2)
+                {
+                    currentClimbFrame = (currentClimbFrame + 1 > climbFrames) ? 1 : currentClimbFrame + 1;
+                }
+                PlayerFrames(2, currentClimbFrame);
+                prevSprite = 2;
+            }
+            else if (dirY == 1)
+            {
+                if (prevSprite == 3)
+                {
+                    currentJumpFrame = (currentJumpFrame + 1 > jumpFrames) ? 1 : currentJumpFrame + 1;
+                }
+                PlayerFrames(3, currentJumpFrame);
+                prevSprite = 3;
+            }
+            else //Idle
+            {
+                PlayerFrames(5, 0);
+            }
         }
+        public void PlayerFrames(int state, int frame)
+        {
+            switch (state)
+            {
+                //Walking right ---->
+                case 0:
+                    switch (frame)
+                    {
+                        case 1:
+                            // ( ._.)
+                            //  /|\
+                            //   |> 
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("( ._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /|", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("  |", ConsoleColor.DarkGray);
+                            WriteColor(">", ConsoleColor.White);
+                                                        
+                            break;
+                        case 2:
+                            // ( ._.)
+                            //  /|\
+                            //  />
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("( ._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /|", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" /", ConsoleColor.DarkGray);
+                            WriteColor(">", ConsoleColor.White);
+                            break;
+                        case 3:
+                            // ( ._.)
+                            //  /|\
+                            //  /\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("( ._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /|", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" /", ConsoleColor.DarkGray);
+                            WriteColor("\\", ConsoleColor.White);
+                            break;
+                        case 4:
+                            // ( ._.)
+                            //  /|\
+                            //   |>
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("( ._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /", ConsoleColor.DarkGray);
+                            WriteColor("|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("  |", ConsoleColor.White);
+                            WriteColor(">", ConsoleColor.DarkGray);
+                            break;
+                        case 5:
+                            // ( ._.)
+                            //  /|\
+                            //  />
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("( ._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /", ConsoleColor.DarkGray);
+                            WriteColor("|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" /", ConsoleColor.White);
+                            WriteColor(">", ConsoleColor.DarkGray);
+                            break;
+                        case 6:
+                            // ( ._.)
+                            //  /|\
+                            //  /\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("( ._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /", ConsoleColor.DarkGray);
+                            WriteColor("|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" /", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            break;
+                        
+                    }
+                    break;
+                //Walking left <-------
+                case 1:
+                    switch (frame)
+                    {
+                        case 1:
+                            // (._. )
+                            //   /|\
+                            //   <| 
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._. )", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("  /|", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("  <", ConsoleColor.DarkGray);
+                            WriteColor("|", ConsoleColor.White);
+                            break;
+                        case 2:
+                            // (._. )
+                            //   /|\
+                            //    <\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._. )", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("  /|", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("   <", ConsoleColor.DarkGray);
+                            WriteColor("\\", ConsoleColor.White);
+                            break;
+                        case 3:
+                            // (._. )
+                            //   /|\
+                            //    /\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._. )", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("  /|", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("   /", ConsoleColor.DarkGray);
+                            WriteColor("\\", ConsoleColor.White);
+                            break;
+                        case 4:
+                            // (._. )
+                            //   /|\
+                            //   <|
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._. )", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("  /", ConsoleColor.DarkGray);
+                            WriteColor("|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("  <", ConsoleColor.White);
+                            WriteColor("|", ConsoleColor.DarkGray);
+                            break;
+                        case 5:
+                            // (._. )
+                            //   /|\
+                            //    <\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._. )", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("  /", ConsoleColor.DarkGray);
+                            WriteColor("|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("   <", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            break;
+                        case 6:
+                            // (._. )
+                            //   /|\
+                            //    /\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._. )", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("  /", ConsoleColor.DarkGray);
+                            WriteColor("|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("   /", ConsoleColor.White);
+                            WriteColor("\\", ConsoleColor.DarkGray);
+                            break;
+                    }
+                    break;
+
+                //Climbing
+                case 2:
+
+                    switch(frame)
+                    {
+                        case 1:
+                            // ('-')
+                            //  /|^
+                            //  ^ \
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("('-')", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /|^", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" ^ \\", ConsoleColor.White);
+                            break;
+
+                        case 2:
+                            // ('-')
+                            //  ^|\
+                            //  / ^
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("('-')", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" ^|\\", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" / ^", ConsoleColor.White);
+                            break;
+                    }
+                    break;
+
+                //Jumping
+                case 3:
+                    switch (frame)
+                    {
+                        case 1:
+                            // ('-')
+                            //  /|\
+                            //  /\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("('-')", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" /|\\^", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" /\\", ConsoleColor.White);
+                            break;
+
+                        case 2:
+                            // (._.)
+                            //  ⁀|⁀
+                            //  ^ ^
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("(._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor(" ⁀|⁀", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor(" ^ ^", ConsoleColor.White);
+                            break;
+
+                        case 3:
+                            // 
+                            // (._.)
+                            // /< >\
+                            Console.SetCursorPosition(X, Y);
+                            WriteColor("", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 1);
+                            WriteColor("(._.)", ConsoleColor.White);
+                            Console.SetCursorPosition(X, Y + 2);
+                            WriteColor("/< >\\", ConsoleColor.White);
+                            break;
+                    }
+                    break;
+                default:
+                    // (._.)
+                    //  /|\
+                    //  /\
+                    Console.SetCursorPosition(X, Y);
+                    WriteColor("(._.)", ConsoleColor.White);
+                    Console.SetCursorPosition(X, Y + 1);
+                    WriteColor(" /|\\", ConsoleColor.White);
+                    Console.SetCursorPosition(X, Y + 2);
+                    WriteColor(" /\\", ConsoleColor.White);
+                    break;
+
+            }
+        }
+
         public void Move(int dx, int dy)
         {
+            int newX = X + (dx * WalkingSpeed) * 2; //Multeplies the direction with the walk speed (-1 * speed) (1 * speed). Adds * 2 cuz a symbal is about twice as tall as it is wide. 
+            int newY = Y + (dy * WalkingSpeed);
 
-            int newX = X + dx;
-            int newY = Y + dy;
-
-            // kolla gränser
-            if (newX >= 0 && newX < Console.WindowWidth &&
-                newY >= 0 && newY < Console.WindowHeight)
+            // Check bounds
+            if (newX >= screenLeft && newX < screenRight - 4 &&
+                newY >= screenTop && newY < screenBottom - 1)
             {
                 X = newX;
                 Y = newY;
